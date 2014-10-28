77392e730d1326d8c77d514ffd0ba77c09f8465e
href <t@x.t>
2014-10-28 21:18:21 +0100

    Second version
    - stations printed in two columns
    - works on windows and rpi

--- radio.py
+++ radio.py
@@ -7,20 +7,63 @@
 # one per line, in the following format:
 # id Name___url of mp3 stream
 #
-import subprocess
+
+import os           # used to clear display
+import subprocess   # used to start another program (mpg123)
+
+# open file and split by lines
 f = open('stations.txt','r')
 lines = f.readlines()
 f.close()
-# print len(lines) # number of items in array
-x=0
-while x < len(lines):
-	print lines[x].strip("\n")
-	x=x+1
-station = input('Enter Station #ID: ')
-print "Selected Station station #ID: ", station
-str = lines[station-1]
-print str
-url = str.split("\t");
-# print "test: ", url[1].strip("\n")
-cmdline = ['mpg123', url[1].strip("\n")]
+
+# setup stations and urls arrays
+stations = []
+urls = []
+for line in lines:
+	split = line.split("\t")
+	stations.append(split[0].strip("\n"))
+	urls.append(split[1].strip("\n"))
+
+# print stations (on Adafruit PiTFT, console)
+
+def display(list):
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print "----------------------=[RpyRadio v1.0]=-"
+	print ""
+	x = -1
+	second = ""
+	for k in list:
+		x=x+1
+		if x == 0:
+			print "", list[0][0:17].ljust(18), "." , list[1][0:17].ljust(18)
+			continue
+		if x == 1:
+			continue
+		if x % 2 == 0:
+			second = k
+			continue
+		print "", second[0:17].ljust(18), "." , k[0:17].ljust(18)
+		second = ""
+	print ""
+	print "----------------------------------------"
+	return
+
+keepasking=1
+
+while keepasking == 1:
+	display(stations)
+	station = input('Enter Station #ID: ')
+	if station > len(stations):
+		continue
+	if station < 0:
+		continue
+	keepasking=0
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# start mpg123
+cmdline = ['mpg123', urls[station-1].strip("\n")]
+if os.name == 'nt':
+	cmdline = ['c:\\Python27\\mpg123\\mpg123.exe', urls[station-1].strip("\n")]
+
 subprocess.call(cmdline)
